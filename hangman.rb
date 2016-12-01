2555ff3ba66a2fae080f316dcc031aee6e8d54b0
Susan Graves <susan@susangravescpa.com>
2016-11-30 19:32:02 -0500

    first real test passing

--- hangman.rb
+++ hangman.rb
@@ -1,8 +1,45 @@
-Class Hangman
-    attr_accessor :word
+class Hangman
+    attr_accessor :word, :secret_word, :guessed_letters, :correct_guesses, :guessed_word
 
-    def initialize(num_of_letters)
-        @word = Array.new(num_of_letters, "")
+
+    def initialize(word)
+        @word = word.downcase!
+        @secret_word = secret_word
+        @guessed_letters = guessed_letters
+        @correct_guesses = correct_guesses
+        @guessed_word = guessed_word
     end
+
+    def input_secret_word(word)
+        secret_word = word.split("")
+    end
+
+    # def create_guessed_word(word)
+    #     guessed_word = Array.new(word.length,"")
+    # end
+
+    # def secret_word_displayed
+    #     number_of_blanks = word.length
+    #     blank_word = Array.new(number_of_blanks, "")
+    # end
+
+    # def word_include?(letter_guess)
+    #     @word.include? letter_guess
+    #     #     true
+    #     # else
+    #     #     false
+    #     # end
+    # end
+
+    # def guessed_letters_array(letter_guess)
+    #     @correct_guesses << letter_guess
+    # end
+
+
+
+
+
+
+    
    
 end
--- test_hangman.rb
+++ test_hangman.rb
@@ -1,9 +1,33 @@
-require 'minitest/autorun'
-require_relative 'app.rb'
+require "minitest/autorun"
+require_relative "hangman.rb"
 
-class TestApp < Minitest::Test
+class TestHangman < Minitest::Test
 
-    def test_input_word_number_of_letters
-        game_word = Hangman.new(4)
-        assert_equal(["","","",""], game_word.word)
+    def test_input_word_returns_array_of_strings
+        word = Hangman.new("debit")
+        assert_equal(["d","e","b","i","t"], word.input_secret_word("debit"))
     end
+
+    # def test_word_returns_empty_array_same_num_strings
+    #     word = Hangman.new("debit")
+    #     assert_equal(["","","","",""], game_word.secret_word_displayed)
+    # end
+
+    # def test_o_is_a_correct_guess
+    #     game_word = Hangman.new("house")
+    #     letter_guess = "o"
+    #     assert_equal(true, game_word.word_include?("o"))
+    # end
+
+    # def test_r_is_a_false_guess
+    #     game_word = Hangman.new("house")
+    #     letter_guess = "r"
+    #     assert_equal(false, game_word.word_include?("r"))
+    # end
+
+    # def test_letter_guess_pushed_into_array
+    #     game_word = Hangman.new("house")
+    #     assert_equal(["o"], game_word.guessed_letter_array("o"))
+    # end
+
+end
