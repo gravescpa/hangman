c2f99c6e03a44cc3ddffade09ff3750b293aa21a
Susan Graves <susan@susangravescpa.com>
2016-12-06 11:04:20 -0500

    home screen & gets player name.  Ready to play game

--- app.rb
+++ app.rb
@@ -6,7 +6,20 @@ enable :sessions
 
 get '/' do
     session[:game] = Hangman.new
+    erb :home, :locals => { :game => session[:game]}
+end
 
-    erb :home, 
+get '/player_1_name' do
+    erb :welcome, :locals => { :game => session[:game], :player_1_name => session[:player_1_name]}
+end
+
+post '/player_1_name' do
+    session[:player_1_name] = params[:player_1]
+    erb :player_1_name, :locals => { :game => session[:game], :player_1_name => session[:player_1_name]}    
+end
+
+get '/play_game' do
+    session[:player_1_name] = params[:player_1]
+    erb :play_game, :locals => { :game => session[:game], :player_1_name => session[:player_1_name]} 
 
 end
